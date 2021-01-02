8d32b70a0cc782279a0078a7df281043a96fa123
mixxamm <maximjanssensj5@gmail.com>
2021-01-02 15:54:33 +0100

    Corrigeer rotation bugs enzo
    Co-authored-by: MrDanaT <dana.tabatabaieirani@gmail.com>

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -39,31 +39,31 @@ namespace Assets.Scripts
             actionsOut[2] = 0f;
             actionsOut[3] = 0f;
             actionsOut[4] = 0f;
+            actionsOut[5] = 0f;
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                Debug.Log("Key UP");
                 actionsOut[0] = 2f;
             }
-            else if (Input.GetKey(KeyCode.DownArrow))
+             if (Input.GetKey(KeyCode.DownArrow))
             {
                 actionsOut[1] = 1f;
             }
-            else if (Input.GetKey(KeyCode.LeftArrow))
+             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 actionsOut[2] = 1f;
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+             if (Input.GetKey(KeyCode.RightArrow))
             {
                 actionsOut[3] = 1f;
             }
-            else if (Input.GetKey(KeyCode.D))
+             if (Input.GetKey(KeyCode.D))
             {
                 actionsOut[4] = 1f;
             }
-            else if (Input.GetKey(KeyCode.A))
+             if (Input.GetKey(KeyCode.A))
             {
-                actionsOut[4] = -1f;
+                actionsOut[5] = 1f;
             }
         }
 
@@ -72,35 +72,33 @@ namespace Assets.Scripts
             if (vectorAction[0] > 0.5f)
             {
                 Vector3 rightVelocity = new Vector3(movementSpeed * vectorAction[0], 0f, 0f);
-                rbody.AddForce(rightVelocity, ForceMode.VelocityChange);
+                rbody.velocity = rightVelocity;
             }
             if (vectorAction[1] > 0.5f)
             {
                 Vector3 leftVelocity = new Vector3(-movementSpeed * vectorAction[1], 0f, 0f);
-                rbody.AddForce(leftVelocity, ForceMode.VelocityChange);
+                rbody.velocity = leftVelocity;
             }
             if (vectorAction[2] > 0.5f)
             {
                 Vector3 rightVelocity = new Vector3(0f, 0f, movementSpeed * vectorAction[2]);
-                rbody.AddForce(rightVelocity, ForceMode.VelocityChange);
+                rbody.velocity = rightVelocity;
             }
             if (vectorAction[3] > 0.5f)
             {
                 Vector3 leftVelocity = new Vector3(0f, 0f, -movementSpeed * vectorAction[3]);
-                rbody.AddForce(leftVelocity, ForceMode.VelocityChange);
+                rbody.velocity = leftVelocity;
             }
 
-            if (vectorAction[4] >= 0.5f)
+            if (vectorAction[4] > 0.5f)
             {
                 transform.Rotate(0f, (2 * rotationSpeed) * Time.deltaTime, 0f);
             }
 
-            if (vectorAction[4] < 0.5f)
+            if (vectorAction[5] > 0.5f)
             {
                 transform.Rotate(0f, (2 * rotationSpeed) * Time.deltaTime * -1, 0f);
             }
-
-            Debug.Log(vectorAction[4]);
         }
 
         protected virtual void OnCollisionEnter(Collision collision)
